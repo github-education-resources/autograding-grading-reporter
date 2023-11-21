7b0f9910b89c425276f17ded3e58b5d60a47a0ab
Jess Tran <jalafel@github.com>
2023-11-20 17:59:02 -0800

    create a aggregate function

---
+++ src/aggregate-results.js
@@ -0,0 +1,73 @@
+const { COLORS } = require("./colors");
+const Table = require("cli-table3");
+
+const getTestScore = (results) => {
+  return results.tests.reduce((acc, { status }) => {
+    if (status === "pass") {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+};
+
+const getAllMaxScores = (runnerResults) => {
+  return runnerResults.reduce((acc, { results }) => {
+    return acc + results.max_score;
+  }, 0);
+};
+
+const getMaxScore = (results) => {
+  return results.max_score || 0;
+};
+
+const getWeight = (maxScore, allMaxScores) => {
+  if (maxScore === 0) {
+    return (0).toFixed(2);
+  }
+  const weight = allMaxScores !== 0 ? (maxScore / allMaxScores) * 100 : 0;
+  return Math.round(weight).toFixed(2);
+};
+
+function AggregateResults(runnerResults) {
+  const table = new Table({
+    head: ["Test Runner Name", "Test Score", "Max Score", "Weight"],
+    colWidths: [20, 13, 13, 10],
+  });
+
+  const allMaxScores = getAllMaxScores(runnerResults);
+
+  console.log(COLORS.magenta, "Test Runner Summary", COLORS.reset);
+
+  let totals = [
+    {
+      score: 0,
+      maxScore: 0,
+      weight: 0,
+    },
+  ];
+
+  runnerResults.forEach(({ runner, results }) => {
+    const maxScore = getMaxScore(results);
+    const weight = getWeight(maxScore, allMaxScores);
+    const score = getTestScore(results);
+
+    table.push([runner.trim(), score, maxScore, weight]);
+    totals.push({
+      score,
+      maxScore,
+      weight,
+    });
+  });
+
+  table.push([
+    "Total: ",
+    "----",
+    "----",
+    totals.reduce((acc, { score, weight, maxScore }) => {
+      return acc + (score / (maxScore || 1)) * weight;
+    }, 0) + "%",
+  ]);
+  console.log(table.toString());
+}
+
+exports.AggregateResults = AggregateResults;
--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
-const core = require('@actions/core')
-const { ConsoleResults } = require('./console-results')
-const { NotifyClassroom } = require('./notify-classroom')
+const core = require("@actions/core");
+const { ConsoleResults } = require("./console-results");
+const { NotifyClassroom } = require("./notify-classroom");
+const { AggregateResults } = require("./aggregate-results");
 
 try {
   const runnerResults = core
-    .getInput('runners')
-    .split(',')
+    .getInput("runners")
+    .split(",")
     .map((runner) => {
-      const encodedResults = process.env[`${runner.toUpperCase()}_RESULTS`]
-      const json = Buffer.from(encodedResults, 'base64').toString('utf-8')
-      return { runner, results: JSON.parse(json) }
-    })
+      const encodedResults = process.env[`${runner.trim().toUpperCase()}_RESULTS`];
+      const json = Buffer.from(encodedResults, "base64").toString("utf-8");
+      return { runner, results: JSON.parse(json) };
+    });
 
-  ConsoleResults(runnerResults)
-  NotifyClassroom(runnerResults)
+  ConsoleResults(runnerResults);
+  NotifyClassroom(runnerResults);
+  AggregateResults(runnerResults);
 
-  if (runnerResults.some((r) => r.results.status === 'fail')) {
-    core.setFailed('Some tests failed.')
+  if (runnerResults.some((r) => r.results.status === "fail")) {
+    core.setFailed("Some tests failed.");
   }
 } catch (error) {
-  core.setFailed(error.message)
+  core.setFailed(error.message);
 }
