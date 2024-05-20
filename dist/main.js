5e58a26c2b01b3e405f0b90248e8825d24bce566
JakeHamblin <jake@jakehamblin.com>
2024-05-20 14:10:08 -0400

    New compile

--- dist/main.js
+++ dist/main.js
@@ -32797,14 +32797,13 @@ try {
     core.setFailed("Some tests errored.");
   }
 } catch (error) {
+  console.log(error);
+  console.log(error.message);
   const input = core.getInput("runners");
   const pattern = /^([a-zA-Z0-9]+,)*[a-zA-Z0-9]+$/
   if (!pattern.test(input)) {
     console.error("The runners input must be a comma-separated list of strings.");
     core.setFailed("The runners input must be a comma-separated list of strings.");
-  } else {
-    console.error(error.message)
-    core.setFailed(error.message);
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -24,13 +24,12 @@ try {
     core.setFailed("Some tests errored.");
   }
 } catch (error) {
+  console.log(error);
+  console.log(error.message);
   const input = core.getInput("runners");
   const pattern = /^([a-zA-Z0-9]+,)*[a-zA-Z0-9]+$/
   if (!pattern.test(input)) {
     console.error("The runners input must be a comma-separated list of strings.");
     core.setFailed("The runners input must be a comma-separated list of strings.");
-  } else {
-    console.error(error.message)
-    core.setFailed(error.message);
   }
 }
